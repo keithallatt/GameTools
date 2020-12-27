d67a112f079917165ca9ce03e5e395e8cdd87049
Keith Allatt <keith.allatt@gmail.com>
2020-12-27 01:40:59 -0500

    Added initial game engine checks, making sure all imports are installed, and being able to collect user input without using builtin input()

---
+++ GameSystem/import_support.py
@@ -0,0 +1,67 @@
+import subprocess
+import sys
+import os
+import re
+
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+
+def install_all_packages(modules_to_try):
+    successful = True
+    for module in modules_to_try:
+        try:
+            __import__(module)
+        except ImportError as e:
+            successful = False
+            print("Failed to import", module, "\nAttempting to install.")
+            install(e.name)
+    if successful:
+        print("All modules are installed.")
+
+
+def find_all_packages():
+    # since in a sub-folder
+    working_dir = os.sep.join(os.getcwd().split(os.sep)[:-1]) + os.sep
+
+    def add_module_path(sd):
+        return working_dir + sd + os.sep + "__init__.py"
+
+    folders = [
+        working_dir+subdir for subdir in os.listdir(working_dir)
+        if os.path.exists(add_module_path(subdir))
+    ]
+
+    import_regex = re.compile(r'^\s*((import (\w+))|(from ([\w\.]+) import (\w+|\*)))$')
+
+    modules_to_import = set()
+
+    for submodule in folders:
+        files = os.listdir(submodule)
+        for file in files:
+            if file == "__pycache__" or file.startswith("."):
+                continue
+
+            contents = open(submodule + os.sep + file, 'r').readlines()
+
+            for line in contents:
+                m = import_regex.match(line)
+                if m is not None:
+                    # group 3 and group 5 contain module for either type of import
+                    # don't add imports within this project
+                    # (such as InventorySystem.inventory or the like)
+                    import_module = m.group(3) or m.group(5)
+                    for package in folders:
+                        package_name = package.split(os.sep)[-1]
+                        if package_name in import_module:
+                            break
+                    else:
+                        modules_to_import.add(import_module)
+
+    return list(modules_to_import)
+
+
+if __name__ == "__main__":
+    modules = find_all_packages()
+    install_all_packages(modules)
---
+++ GameSystem/raw_input_test.py
@@ -0,0 +1,51 @@
+""" Seems to work! """
+
+from pynput import keyboard
+
+options = [
+    "Yes", "No", "Maybe"
+]
+
+index = 0
+
+def option_str():
+    global index, options
+    return "   ".join(
+        [
+            "<" + options[i] + ">" if i == index else " " + options[i] + " "
+            for i in range(len(options))
+        ]
+    )
+
+def on_press(key):
+    global index, options
+    try:
+        if key == keyboard.Key.left:
+            index = max(index-1, 0)
+        if key == keyboard.Key.right:
+            index = min(index+1, len(options)-1)
+        print('\r' + option_str(), end="")
+    except AttributeError:
+        print('\rspecial key {0} pressed'.format(
+            key), end="")
+
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+print('\r' + option_str(), end="")
+
+# Collect events until released
+with keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release, suppress=True) as listener:
+    listener.join()
+
+# ...or, in a non-blocking fashion:
+#listener = keyboard.Listener(
+#    on_press=on_press,
+#    on_release=on_release,
+#    suppress=True)
+#listener.start()
